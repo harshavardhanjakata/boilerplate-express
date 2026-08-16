b372cc63f4564aa0dab9554126af8279dd7b599a
harsha vardhan <harshavardhanjakata67@gmail.com>
2026-08-16 21:39:54 +0530

    16aug

--- myApp.js
+++ myApp.js
@@ -3,9 +3,10 @@ let app = express();
 
 console.log('Hello World');
 app.get('/',(req,res) => {
-    res.send("Hello Express");
+    // res.send("Hello Express");
+    res.sendFile(__dirname+'/views/index.html')
 });
-
+app.use('/public',express.static(__dirname + '/public'));
 
 
 
